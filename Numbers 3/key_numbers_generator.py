b08d797d54eb9606e100e1ed6efda3e83b9be8b5
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-10-15 23:58:40 +1100

    add: Filter out duplicate keys

--- Numbers 3/key_numbers_generator.py
+++ Numbers 3/key_numbers_generator.py
@@ -2,16 +2,20 @@ from itertools import product
 from typing import Iterable
 
 
-def numbers_permutations(initial_numbers: list[int], length: int) -> Iterable[tuple[int]]:
+def generate_key_numbers(initial_numbers: list[int], length: int=12) -> Iterable[tuple[int]]:
     numbers = initial_numbers + [0] * (length - len(initial_numbers))
     # todo: Handle case where there are more initial numbers than the length. In that case return permutations of adding
     #  each extra numbers to each other until at the length limit
 
-    return product(numbers, repeat=length)
+    # todo: If we could generate the key numbers without duplicates in the first place, that would be better than
+    #  filtering them as they're generated as it will greatly reduce memory usage
+    # Potentially high memory usage, upper bound is exponential O(2^n)
+    seen_key_numbers = set()
+    for p in product(numbers, repeat=length):
+        if p not in seen_key_numbers:
+            seen_key_numbers.add(p)
+            yield p
 
 if __name__ == "__main__":
-    seen = set()
-    for p in numbers_permutations(list(range(6)), 12):
-        if p not in seen:
-            seen.add(p)
-            print(p)
+    for p in generate_key_numbers(list(range(6))):
+        print(p)
