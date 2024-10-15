7360e91a50535ef64a6626413759e751bee829c3
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-10-15 23:55:44 +1100

    chg: Generate cartesian product not permutations

--- Numbers 3/key_numbers_generator.py
+++ Numbers 3/key_numbers_generator.py
@@ -1,11 +1,13 @@
-from itertools import permutations
+from itertools import product
 from typing import Iterable
 
 
 def numbers_permutations(initial_numbers: list[int], length: int) -> Iterable[tuple[int]]:
     numbers = initial_numbers + [0] * (length - len(initial_numbers))
+    # todo: Handle case where there are more initial numbers than the length. In that case return permutations of adding
+    #  each extra numbers to each other until at the length limit
 
-    return permutations(numbers, length)
+    return product(numbers, repeat=length)
 
 if __name__ == "__main__":
     seen = set()
