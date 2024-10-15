4164d9088cd478032415bfd51ecaf4536ca38876
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-10-16 01:37:43 +1100

    add: Enable numbers other than 0 to be fillers

--- Numbers 3/key_numbers_generator.py
+++ Numbers 3/key_numbers_generator.py
@@ -1,12 +1,13 @@
-from itertools import product
+from itertools import permutations, product
 from typing import Iterable
 
 
-def generate_key_numbers(initial_numbers: list[int], length: int=12) -> Iterable[tuple[int]]:
+def generate_key_numbers(initial_numbers: list[int], length: int=12, filler_numbers: Iterable=None) -> Iterable[tuple[int]]:
+    if filler_numbers is None:
+        filler_numbers = [0]
     # Increases the chance of same key numbers being generated and filtered
-    numbers = [n % 6 for n in initial_numbers]
+    initial_numbers = [n % 6 for n in initial_numbers]
 
-    numbers = numbers + [0] * (length - len(numbers))
     # todo: Handle case where there are more initial numbers than the length. In that case return permutations of adding
     #  each extra numbers to each other until at the length limit
 
@@ -14,11 +15,13 @@ def generate_key_numbers(initial_numbers: list[int], length: int=12) -> Iterable
     #  filtering them as they're generated as it will greatly reduce memory usage
     # Potentially high memory usage, upper bound is exponential O(2^n)
     seen_key_numbers = set()
-    for p in product(numbers, repeat=length):
-        if p not in seen_key_numbers:
-            seen_key_numbers.add(p)
-            yield p
+    for filler in product(filler_numbers, repeat=length - len(initial_numbers)):
+        numbers = initial_numbers + list(filler)
+        for p in permutations(numbers, length):
+            if p not in seen_key_numbers:
+                seen_key_numbers.add(p)
+                yield p
 
 if __name__ == "__main__":
-    for p in generate_key_numbers(list(range(6))):
+    for p in generate_key_numbers(list(range(2)), length=5, filler_numbers=[2, 3]):
         print(p)
