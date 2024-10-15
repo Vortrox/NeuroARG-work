2a041e4607dd3e93250a5be0d758e548718c106e
Vortrox <39481603+Vortrox@users.noreply.github.com>
2024-10-15 23:16:40 +1100

    ref: Use built in permutations generator

--- Numbers 3/key_numbers_combinations.py
+++ Numbers 3/key_numbers_combinations.py
@@ -1,26 +1,15 @@
-def combinations(initial_numbers: list[int], length: int) -> list[list[int]]:
-    def h(combination: list[int], remaining_numbers: list[int]):
-        if len(combination) == length:
-            yield combination
+from itertools import permutations
+from typing import Iterable
 
-        seen_combos = set()
 
-        for i, number in enumerate(remaining_numbers):
-            remaining_numbers_excluding_chosen_number = remaining_numbers[0:i] + remaining_numbers[i+1:len(remaining_numbers)]
-            partial_combination = combination + [number]
+def numbers_permutations(initial_numbers: list[int], length: int) -> Iterable[tuple[int]]:
+    numbers = initial_numbers + [0] * (length - len(initial_numbers))
 
-            # Prevents duplicated combinations
-            if tuple(partial_combination) in seen_combos:
-                continue
-            seen_combos.add(tuple(partial_combination))
-
-            for combo in h(partial_combination, remaining_numbers_excluding_chosen_number):
-                yield combo
-
-    number_choices = initial_numbers + [0] * (length - len(initial_numbers))
-    combos = [c for c in h([], number_choices)]
-
-    return combos
+    return permutations(numbers, length)
 
 if __name__ == "__main__":
-    print(*combinations([1, 2, 3], 5), sep="\n")
+    seen = set()
+    for p in numbers_permutations(list(range(6)), 12):
+        if p not in seen:
+            seen.add(p)
+            print(p)
